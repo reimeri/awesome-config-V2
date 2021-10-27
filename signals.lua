9fed1956f753a79753820fdbf9ca8b837a1a9af4
reimeri <reimeri@tuta.io>
2021-10-28 01:01:03 +0300

    Hide borders automatically when only 1 window open on screen

--- signals.lua
+++ signals.lua
@@ -65,9 +65,23 @@ function signals:init(args)
         c:emit_signal("request::activate", "mouse_enter", {raise = false})
     end)
     
-    client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+    client.connect_signal("focus", function(c) 
+	border_adjust(c)
+    end)
     client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
     -- }}}
+end
+
+-- No border for maximized clients or when only 1 client visible
+function border_adjust(c)
+    if c.maximized then
+	c.border_width = 0
+    elseif #awful.screen.focused().clients == 1 then
+        c.border_width = 0
+    elseif #awful.screen.focused().clients > 1 then
+        c.border_width = beautiful.border_width
+        c.border_color = beautiful.border_focus
+    end
 end
 
 return signals
